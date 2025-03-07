f1a73e9b7221b1d53dcbf0c62ccb17856335826e
Krish Manek <krishmanek2003@gmail.com>
2025-03-07 17:59:43 +0530

    [Add] Added day one task files

---
+++ school_management/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name':'School Management',
+    'desc':'School Management',
+    'author':'Krish Manek',
+    'depends':['base'],
+    'sequence':1,
+    'version':'1.0',
+    'data':[
+        'security/ir.model.access.csv',
+        'views/school_view.xml'
+    ],
+}
---
+++ school_management/models/school.py
@@ -0,0 +1,6 @@
+from odoo import fields, models
+class SchoolDetails(models.Model):
+    _name = "school.details"
+    _description = "School_Details"
+
+    name = fields.Char(String="name")
