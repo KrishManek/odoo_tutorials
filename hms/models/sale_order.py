2ee65395ca498a47b1b69acea147fc0b9a7af55d
Krish Manek <krishmanek2003@gmail.com>
2025-04-08 11:38:06 +0530

    [upd] improved code

--- hms/models/sale_order.py
+++ hms/models/sale_order.py
@@ -7,10 +7,11 @@ class SaleOrder(models.Model):
     lead_reference = fields.Char(string="Lead Reference")
     
     @api.model_create_multi
-    def create(self, val_lists):
-        partner_id = val_lists.get('partner_id')
-        if partner_id:
-            partner = self.env['res.partner'].browse(partner_id)
-            if partner.use_customers_tc and partner.terms_and_conditions:
-                val_lists['note'] = partner.terms_and_conditions
-        return super(SaleOrder, self).create(val_lists)
+    def create(self, vals_list):
+        for vals in vals_list:
+            partner_id = vals.get('partner_id')
+            if partner_id:
+                partner = self.env['res.partner'].browse(partner_id)
+                if partner.use_customers_tc and partner.terms_and_conditions:
+                    vals['note'] = partner.terms_and_conditions
+        return super(SaleOrder, self).create(vals_list)
