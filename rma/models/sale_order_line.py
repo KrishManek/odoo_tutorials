a4e12b65bbc44ab3fd7400f90f2860caef37c9fd
Krish Manek <krishmanek2003@gmail.com>
2025-05-08 11:46:11 +0530

    [upd] updated and optimized code also updated location in delivery

--- rma/models/sale_order_line.py
+++ rma/models/sale_order_line.py
@@ -4,36 +4,31 @@ from odoo import fields, models, api
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    child_warehouse_id = fields.Many2one('stock.location', string='Child Warehouse')
+    location_id = fields.Many2one('stock.location', string='Warehouse Location')
     
     def _get_procurement_group(self):
         self.ensure_one()
-        if self.child_warehouse_id:
-            domain = [
-                ('sale_id', '=', self.order_id.id),
-                ('child_warehouse_id', '=', self.child_warehouse_id.id),
-            ]
-            return self.env['procurement.group'].search(domain, limit=1)
+        if self.location_id:
+            return self.env['procurement.group'].search([
+                                                    ('sale_id', '=', self.order_id.id),
+                                                    ('location_id', '=', self.location_id.id),
+                                                ], limit=1)
         return self.order_id.procurement_group_id
 
     def _prepare_procurement_group_vals(self):
-        self.ensure_one()
-        if self.child_warehouse_id:
-            return {
-                'name': f"{self.order_id.name} - {self.child_warehouse_id.display_name}",
-                'move_type': self.order_id.picking_policy,
-                'sale_id': self.order_id.id,
-                'partner_id': self.order_id.partner_shipping_id.id,
-                'child_warehouse_id': self.child_warehouse_id.id,
-            }
-        return super(SaleOrderLine,self)._prepare_procurement_group_vals()
+        values = super(SaleOrderLine,self)._prepare_procurement_group_vals()
+        if self.location_id:
+            self.ensure_one()
+            values.update({
+                'location_id': self.location_id.id,
+            })
+        return values
         
     def _prepare_procurement_values(self, group_id=False):
         values = super(SaleOrderLine, self)._prepare_procurement_values(group_id)
-        if self.child_warehouse_id:
+        if self.location_id:
             self.ensure_one()
             values.update({
-                'warehouse_id': self.order_id.warehouse_id,
-                'child_warehouse_id': self.child_warehouse_id,
+                'location_id': self.location_id.id,       
             })
         return values
