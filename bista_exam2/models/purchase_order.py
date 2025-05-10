b4346afe1a7018a69488aea21241ebffd4b62c5c
Krish Manek <krishmanek2003@gmail.com>
2025-05-10 11:51:02 +0530

    [upd] Updated exam code

--- bista_exam2/models/purchase_order.py
+++ bista_exam2/models/purchase_order.py
@@ -1,14 +1,14 @@
 from odoo import fields,models,api
 from odoo.exceptions import UserError
 
-class Product(models.Model):
+class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
     
     def button_confirm(self):
         result = super().button_confirm()
         if self.partner_id.email:
             template_id = self.env.ref('bista_exam2.email_template_new_purchase_order')
-            template_id.send_mail(self.id)
+            template_id.send_mail(self.id, force_send=True)
         return result
     
     
